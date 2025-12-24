febf2e298aa6b229a8e72d9d7de920af7426b8dc
dagota12 <chernetdagim@gmail.com>
2025-12-24 12:38:32 +0300

    chore: update .gitignore to include clerk configuration and modify auth.config.ts to use environment variable for domain

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://heroic-crow-73.clerk.accounts.dev",
+      domain: process.env.CLERK_JWT_ISSUER_DOMAIN!,
       applicationID: "convex",
     },
   ],
